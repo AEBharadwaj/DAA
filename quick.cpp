4b9553014a9f838bba5e4e53352a66842ba2d272
AEBharadwaj <thekanhaiya009@gmail.com>
2026-07-31 09:29:48 +0530

    quick.cpp

--- quick.cpp
+++ quick.cpp
@@ -1,50 +1,43 @@
 #include <iostream>
 using namespace std;
 
-// Partition function
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high];   // last element as pivot
-    int i = low - 1;         // boundary of smaller elements
+    int pivot = arr[high];
+    int i = low - 1;
 
     for (int j = low; j < high; j++) {
-        if (arr[j] <= pivot) {
+        if (arr[j] < pivot) {
             i++;
-            swap(arr[i], arr[j]);   // move smaller to left
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
 
-    swap(arr[i + 1], arr[high]);    // place pivot correctly
-    return i + 1;                    // return pivot index
+    return i + 1;
 }
 
-// Quick Sort function
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
-        int pivotIndex = partition(arr, low, high);
-
-        quickSort(arr, low, pivotIndex - 1);    // sort left
-        quickSort(arr, pivotIndex + 1, high);   // sort right
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
-}
-
-// Print array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
 }
 
 int main() {
-    int arr[] = {3, 6, 8, 10, 1, 2, 1};
-    int n = 7;
-
-    cout << "Before sorting: ";
-    printArray(arr, n);
+    int arr[] = {64, 25, 12, 22, 11, 90, 45, 33, 7, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
     quickSort(arr, 0, n - 1);
 
-    cout << "After sorting:  ";
-    printArray(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 
     return 0;
 }
+
