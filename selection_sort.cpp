d9cf9a4b6c5ed1050a450669b4dd9f85c73c257b
AEBharadwaj <thekanhaiya009@gmail.com>
2026-07-31 09:28:45 +0530

    selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIdx];
+        arr[minIdx] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11, 90, 45, 33, 7, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
 
