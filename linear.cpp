2ca0c9da590e8812875ff323e828662195ab8166
AEBharadwaj <thekanhaiya009@gmail.com>
2026-07-31 09:24:59 +0530

    linear.cpp

--- linear.cpp
+++ linear.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;        // found at index i
+        }
+    }
+    return -1;               // not found
+}
+
+int main() {
+    int arr[] = {3, 6, 8, 10, 1, 2, 1};
+    int n = 7;
+    int target = 10;
+
+    int result = linearSearch(arr, n, target);
+
+    if (result != -1)
+        cout << "Found at index: " << result << endl;
+    else
+        cout << "Not found" << endl;
+
+    return 0;
+}
 
